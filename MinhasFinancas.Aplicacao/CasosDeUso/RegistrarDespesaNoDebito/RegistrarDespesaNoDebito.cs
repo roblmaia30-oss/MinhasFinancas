14f0c4b79b97f1d68f9af31957fb32804089b72d
Robson Maia <roblmaia30@gmail.com>
2026-04-25 02:40:13 -0300

    feat: implementa Casos de Uso de despesa e interfaces de repositorio

---
+++ MinhasFinancas.Aplicacao/CasosDeUso/RegistrarDespesaNoDebito/RegistrarDespesaNoDebito.cs
@@ -0,0 +1,30 @@
+﻿using MinhasFinancas.Aplicacao.Interfaces;
+using MinhasFinancas.Dominio.Entidades;
+
+namespace MinhasFinancas.Aplicacao.CasosDeUso.RegistrarDespesaNoDebito;
+
+public class RegistrarDespesaNoDebito(IContaRepositorio contaRepositorio, ITransacaoRepositorio transacaoRepositorio)
+{
+    private readonly IContaRepositorio _contaRepositorio = contaRepositorio;
+    private readonly ITransacaoRepositorio _transacaoRepositorio = transacaoRepositorio;
+
+    public async Task Executar(RegistrarDespesaNoDebitoRequest request)
+    {
+        var conta = await _contaRepositorio.ObterPorId(request.ContaId) ?? throw new Exception("Conta de débito não encontrada!");
+
+        conta.Debitar(request.Valor);
+
+        var transacao = new Transacao(
+            descricao: request.Descricao,
+            valor: request.Valor,
+            data: request.Data,
+            tipo: TipoTransacao.Despesa,
+            metodo: MetodoPagamento.Debito,
+            contaId: conta.Id,
+            categoriaId: request.CategoriaId
+        );
+
+        await _contaRepositorio.Atualizar(conta);
+        await _transacaoRepositorio.Adicionar(transacao);
+    }
+}
---
+++ MinhasFinancas.Aplicacao/CasosDeUso/RegistrarDespesaNoDebito/RegistrarDespesaNoDebitoRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MinhasFinancas.Aplicacao.CasosDeUso.RegistrarDespesaNoDebito;
+
+public record RegistrarDespesaNoDebitoRequest(
+    string Descricao,
+    decimal Valor,
+    Guid ContaId,
+    Guid CategoriaId,
+    DateTime Data
+);
